232f8417ad51672ae54d5502482deb164805f75b
pvvm <pv.mizuno@hotmail.com>
2019-10-18 11:15:18 -0300

    Corrigidos erros do valgrind

--- source/pre_proc.c
+++ source/pre_proc.c
@@ -29,6 +29,7 @@ const char * pre_processamento(char *nome_arq){
             break;
 	    }
     }
+
     strcpy(aux, nome_arq);
     strcat(aux, "pre");
 
@@ -40,14 +41,18 @@ const char * pre_processamento(char *nome_arq){
     }
 
     char pen_token[50], linha[300], str[50], antepen_token[50];
+    strcpy(pen_token, "undefined");
+    strcpy(antepen_token, "undefined");
     char *equ[10], *equ_trocado[10];
     for (int k = 0; k < 10; k++) {
         equ[k] = (char*)malloc(50*sizeof(char));
         strcpy(equ[k], "undefined");
+        strcat(equ[k], "\0");
         equ_trocado[k] = (char*)malloc(50*sizeof(char));
         strcpy(equ_trocado[k], "undefined");
+        strcat(equ_trocado[k], "\0");
     }
-    int equ_total = 0, num_linha = 1, flag_enter = 0, f = 0, num_token = 0;
+    int equ_total = 0, num_linha = 1, flag_enter = 0, f = 0, num_token = 0, if_aux = 0, cont = 0;
     int flag_comment = 0, flag_if = 0, flag_section = 0, flag_if_print = 0, flag_pula_linha = 0, flag_espaco = 0;
 
     while(fgets(linha, sizeof(linha), arq)){
@@ -55,7 +60,6 @@ const char * pre_processamento(char *nome_arq){
         flag_enter = 0;
         char *token = strtok(linha, " \n\t");	// Separa cada token por espaco, nova linha e tab
         while(token){
-
             /*Tratamento de Comentários*/
             if(token[0] == ';')     // Retira comentários no formato: token ;comentario / ; comentario
                 break;
@@ -82,8 +86,12 @@ const char * pre_processamento(char *nome_arq){
                 flag_enter++;
             }
             for(int j = 0; j < 50; j++){
-                if(token[j] == ':' && num_token == 0)
+                if(token[j] == '\0')
+                    break;
+                if(token[j] == ':' && num_token == 0){
                     flag_enter = 1;
+                    break;
+                }
             }
 
             /*Tratamento de EQU*/
@@ -101,6 +109,8 @@ const char * pre_processamento(char *nome_arq){
                 equ_total++;
                 int tem_dp = 0;
                 for(int i = 0; i < 50; i++){
+                    if(antepen_token[i] == '\0')
+                        break;
                     if(antepen_token[i] == ':')
                         tem_dp = 1;
                 }
@@ -115,7 +125,7 @@ const char * pre_processamento(char *nome_arq){
                     f = 1;                  /*Tentar corrigir o erro do if com label e enter*/
                     break;
                 }
-                for(size_t i = 0; i < strlen(token); i++){
+                for(int i = 0; i < strlen(token); i++){
                     if(token[i] == ':' && num_token == 0){
                         flag_if = 2;
                     }
